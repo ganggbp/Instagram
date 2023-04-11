d60e1a54755d8143bbc7014f458cdfbead89fa67
gangg <burit_bk@hotmail.com>
2023-04-11 15:04:42 +0100

    finish camera ui and feature on android

--- src/screens/PostUploadScreen/PostUploadScreen.tsx
+++ src/screens/PostUploadScreen/PostUploadScreen.tsx
@@ -1,12 +1,204 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, StyleSheet, Pressable} from 'react-native';
+import React, {useEffect, useState, useRef} from 'react';
+import {
+  Camera,
+  CameraType,
+  FlashMode,
+  CameraRecordingOptions,
+  CameraPictureOptions,
+  VideoQuality,
+} from 'expo-camera';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+import colors from '../../theme/colors';
+
+const flashMode = [
+  FlashMode.off,
+  FlashMode.on,
+  FlashMode.auto,
+  FlashMode.torch,
+];
+
+const flashModeToIcon = {
+  [FlashMode.off]: 'flash-off',
+  [FlashMode.on]: 'flash-on',
+  [FlashMode.auto]: 'flash-auto',
+  [FlashMode.torch]: 'highlight',
+};
 
 const PostUploadScreen = () => {
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [cameraType, setCameraType] = useState(CameraType.back);
+  const [flash, setFlash] = useState(FlashMode.off);
+  const [isCameraReady, setIsCameraReady] = useState(false);
+  const [isRecording, setIsRecording] = useState(false);
+
+  const camera = useRef<Camera>(null);
+
+  useEffect(() => {
+    const getPermission = async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const microphonePermission =
+        await Camera.requestMicrophonePermissionsAsync();
+
+      setHasPermission(
+        cameraPermission.status === 'granted' &&
+          microphonePermission.status === 'granted',
+      );
+    };
+
+    getPermission();
+  }, []);
+
+  const flipCamera = () => {
+    setCameraType(currentCameraType =>
+      currentCameraType === CameraType.back
+        ? CameraType.front
+        : CameraType.back,
+    );
+  };
+
+  const flipFlash = () => {
+    const currentIndex = flashMode.indexOf(flash);
+    const nextIndex =
+      currentIndex === flashMode.length - 1 ? 0 : currentIndex + 1;
+    setFlash(flashMode[nextIndex]);
+  };
+
+  const takePicture = async () => {
+    if (!isCameraReady || !camera.current || isRecording) {
+      return;
+    }
+
+    const options: CameraPictureOptions = {
+      quality: 0.5,
+      base64: false,
+      skipProcessing: true,
+    };
+
+    try {
+      const result = await camera.current.takePictureAsync(options);
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const startRecording = async () => {
+    if (!isCameraReady || !camera.current || isRecording) {
+      return;
+    }
+
+    const options: CameraRecordingOptions = {
+      quality: VideoQuality['720p'],
+      maxDuration: 60,
+      maxFileSize: 10 * 1024 * 1024,
+      mute: false,
+    };
+
+    setIsRecording(true);
+
+    try {
+      const result = await camera.current.recordAsync(options);
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+
+    setIsRecording(false);
+  };
+
+  const stopRecording = () => {
+    if (isRecording) {
+      camera.current?.stopRecording();
+      setIsRecording(false);
+    }
+  };
+
+  if (hasPermission === null) {
+    return <Text>Loading</Text>;
+  }
+
+  if (hasPermission === false) {
+    return <Text>No access to the camera</Text>;
+  }
+
   return (
-    <View>
-      <Text>PostUploadScreen</Text>
+    <View style={styles.page}>
+      <Camera
+        ref={camera}
+        style={styles.camera}
+        type={cameraType}
+        flashMode={flash}
+        ratio="4:3"
+        onCameraReady={() => setIsCameraReady(true)}
+      />
+      <View style={[styles.buttonContainer, {top: 25}]}>
+        <MaterialIcons name="close" size={30} color={colors.white} />
+
+        <Pressable onPress={flipFlash}>
+          <MaterialIcons
+            name={flashModeToIcon[flash]}
+            size={30}
+            color={colors.white}
+          />
+        </Pressable>
+        <MaterialIcons name="settings" size={30} color={colors.white} />
+      </View>
+
+      <View style={[styles.buttonContainer, {bottom: 25}]}>
+        <MaterialIcons name="photo-library" size={30} color={colors.white} />
+
+        {isCameraReady && (
+          <Pressable
+            onPress={takePicture}
+            onLongPress={startRecording}
+            onPressOut={stopRecording}>
+            <View
+              style={[
+                styles.circle,
+                {backgroundColor: isRecording ? colors.accent : colors.white},
+              ]}
+            />
+          </Pressable>
+        )}
+
+        <Pressable onPress={flipCamera}>
+          <MaterialIcons
+            name="flip-camera-ios"
+            size={30}
+            color={colors.white}
+          />
+        </Pressable>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: colors.black,
+  },
+  camera: {
+    width: '100%',
+    aspectRatio: 3 / 4,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '100%',
+
+    position: 'absolute',
+  },
+  circle: {
+    width: 75,
+    aspectRatio: 1,
+    borderRadius: 75,
+    backgroundColor: colors.white,
+  },
+});
 
 export default PostUploadScreen;
