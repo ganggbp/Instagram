b1dd29746ba0df805338b6c52c6697a369d3a519
gangg <burit_bk@hotmail.com>
2023-04-16 11:04:50 +0100

    finish lambda

---
+++ amplify/backend/function/InstagramPostConfirmation/src/custom.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event, context) => {
+  console.log('Hey, Lambda function is working');
+  console.log(event);
+
+  return event;
+};
